982236fdc2caddafd014be0d423e8ef013d54f9a
WhiteHoryZen <37026947+WhiteHoryZen@users.noreply.github.com>
2020-08-06 17:32:20 -0400

    Finished endGame Screen

--- endscreen/endgame.js
+++ endscreen/endgame.js
@@ -4,19 +4,19 @@ class endGame extends Phaser.Scene {
     }
 
     create() {
-        this.bg6 = this.add.tileSprite(0, 0, 0, 0, "bg6");
-        this.bg5 = this.add.tileSprite(0, 0, config.width, config.height, "bg5");
-        this.bg4 = this.add.tileSprite(0, 0, config.width, config.height, "bg4");
-        this.bg3 = this.add.tileSprite(0, 0, config.width, config.height, "bg3");
-        this.bg2 = this.add.tileSprite(0, 0, config.width, config.height, "bg2");
-        this.bg1 = this.add.tileSprite(0, 0, config.width, config.height, "bg1");
+        this.bg6 = this.add.tileSprite(400, 300, 0, 0, "bg6");
+        this.bg5 = this.add.tileSprite(400, 300, 0, 0, "bg5");
+        this.bg4 = this.add.tileSprite(400, 300, 0, 0, "bg4");
+        this.bg3 = this.add.tileSprite(400, 300, 0, 0, "bg3");
+        this.bg2 = this.add.tileSprite(400, 300, 0, 0, "bg2");
+        this.bg1 = this.add.tileSprite(400, 300, 0, 0, "bg1");
 
-        this.userScore = this.add.text(225, 75, "Your Time: " + (config.timer / 60).toFixed(3), { fontSize: 35 });
-        this.emily = this.add.text();
-        this.matthew = this.add.text();
-        this.jimmy = this.add.text();
-        this.mark = this.add.text();
-        this.aaron = this.add.text();
+        this.userScore = this.add.text(225, 75, "Your Time: " + (config.timer / 60).toFixed(3), { fontSize: 35, fontStyle: 'bold', color: 0xCD5C5C });
+        this.emily = this.add.text(225, 150, "Emily - Hangman", { fontSize: 20, fontStyle: 'bold', color: 0xCD5C5C });
+        this.matthew = this.add.text(225, 225, "Matthew - Start Screen", { fontSize: 20, fontStyle: 'bold', color: 0xCD5C5C });
+        this.jimmy = this.add.text(225, 300, "Jimmy - Flappy Bird", { fontSize: 20, fontStyle: 'bold', color: 0xCD5C5C });
+        this.mark = this.add.text(225, 375, "Mark - Rock, Paper, Scissors", { fontSize: 20, fontStyle: 'bold', color: 0xCD5C5C });
+        this.aaron = this.add.text(225, 450, "Aaron - Main Map", { fontSize: 20, fontStyle: 'bold', color: 0xCD5C5C });
     }
 
     update() {
