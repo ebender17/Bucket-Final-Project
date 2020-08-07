16418f2d4df710887fbb9e8115681448e2a9fea2
WhiteHoryZen <37026947+WhiteHoryZen@users.noreply.github.com>
2020-08-07 13:32:50 -0400

    fixed pathing issues

--- scripts/preloadmain.js
+++ scripts/preloadmain.js
@@ -4,62 +4,62 @@ class preloadmain extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("tiles", "../mainmap/Assets/Tilesets/final_proj.png");
-        this.load.tilemapTiledJSON("map", "../mainmap/Assets/Tilemaps/main.json");
-        this.load.spritesheet("ghost", "../mainmap/assets/sprites/ghost.png", {
+        this.load.image("tiles", "mainmap/Assets/Tilesets/final_proj.png");
+        this.load.tilemapTiledJSON("map", "mainmap/Assets/Tilemaps/main.json");
+        this.load.spritesheet("ghost", "mainmap/assets/sprites/ghost.png", {
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.spritesheet("player-walk-back", "../mainmap/assets/sprites/walk-back-boy.png", {
+        this.load.spritesheet("player-walk-back", "mainmap/assets/sprites/walk-back-boy.png", {
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.spritesheet("player-walk-front", "../mainmap/assets/sprites/walk-front-boy.png", {
+        this.load.spritesheet("player-walk-front", "mainmap/assets/sprites/walk-front-boy.png", {
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.spritesheet("player-walk-left", "../mainmap/assets/sprites/walk-left-boy.png", {
+        this.load.spritesheet("player-walk-left", "mainmap/assets/sprites/walk-left-boy.png", {
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.spritesheet("player-walk-right", "../mainmap/assets/sprites/walk-right-boy.png", {
+        this.load.spritesheet("player-walk-right", "mainmap/assets/sprites/walk-right-boy.png", {
             frameWidth: 16,
             frameHeight: 16
         });
-        this.load.image("player-front", "../mainmap/assets/sprites/boy-front.png");
-        this.load.image("player-back", "../mainmap/assets/sprites/boy-back.png");
-        this.load.image("player-right", "../mainmap/assets/sprites/boy-right.png");
-        this.load.image("player-left", "../mainmap/assets/sprites/boy-left.png");
-        this.load.image("entryway", "../mainmap/assets/sprites/entry.png");
+        this.load.image("player-front", "mainmap/assets/sprites/boy-front.png");
+        this.load.image("player-back", "mainmap/assets/sprites/boy-back.png");
+        this.load.image("player-right", "mainmap/assets/sprites/boy-right.png");
+        this.load.image("player-left", "mainmap/assets/sprites/boy-left.png");
+        this.load.image("entryway", "mainmap/assets/sprites/entry.png");
 
-        this.load.image("path", "../mainmap/assets/sprites/path.png");
-        this.load.audio("music", "../mainmap/assets/gamesong.mp3");
+        this.load.image("path", "mainmap/assets/sprites/path.png");
+        this.load.audio("music", "mainmap/assets/gamesong.mp3");
 
         //Rockpaperscissors assets
-        this.load.image("background", "../rockpaperscissors/assets/background.jpg");
-        this.load.image("rock", "../rockpaperscissors/assets/rock.png");
-        this.load.image("paper", "../rockpaperscissors/assets/paper.png");
-        this.load.image("scissors", "../rockpaperscissors/assets/scissors.png");
-        this.load.image("questionmark", "../rockpaperscissors/assets/questionmark.png");
-        this.load.image("winScreen", "../rockpaperscissors/assets/winScreen.png");
-        this.load.image("loseScreen", "../rockpaperscissors/assets/loseScreen.png");
+        this.load.image("background", "rockpaperscissors/assets/background.jpg");
+        this.load.image("rock", "rockpaperscissors/assets/rock.png");
+        this.load.image("paper", "rockpaperscissors/assets/paper.png");
+        this.load.image("scissors", "rockpaperscissors/assets/scissors.png");
+        this.load.image("questionmark", "rockpaperscissors/assets/questionmark.png");
+        this.load.image("winScreen", "rockpaperscissors/assets/winScreen.png");
+        this.load.image("loseScreen", "rockpaperscissors/assets/loseScreen.png");
 
 
         //Hangman assets 
-        this.load.bitmapFont("pixelFont", "../hangman/assets/fonts/font.png", "../hangman/assets/fonts/font.xml");
+        this.load.bitmapFont("pixelFont", "hangman/assets/fonts/font.png", "../hangman/assets/fonts/font.xml");
 
-        this.load.image("hangman_background", "../hangman/assets/background/parallax-demon-woods-bg.png");
-        this.load.image("far_trees", "../hangman/assets/background/parallax-demon-woods-far-trees.png");
-        this.load.image("mid_trees", "../hangman/assets/background/parallax-demon-woods-mid-trees.png");
-        this.load.image("close_trees", "../hangman/assets/background/parallax-demon-woods-close-trees.png");
+        this.load.image("hangman_background", "hangman/assets/background/parallax-demon-woods-bg.png");
+        this.load.image("far_trees", "hangman/assets/background/parallax-demon-woods-far-trees.png");
+        this.load.image("mid_trees", "hangman/assets/background/parallax-demon-woods-mid-trees.png");
+        this.load.image("close_trees", "hangman/assets/background/parallax-demon-woods-close-trees.png");
 
         //End Screen assets
-        this.load.image("bg1", "../endscreen/assets/landscape_0000_1_trees.png");
-        this.load.image("bg2", "../endscreen/assets/landscape_0001_2_trees.png");
-        this.load.image("bg3", "../endscreen/assets/landscape_0002_3_trees.png");
-        this.load.image("bg4", "../endscreen/assets/landscape_0003_4_mountain.png");
-        this.load.image("bg5", "../endscreen/assets/landscape_0004_5_clouds.png");
-        this.load.image("bg6", "../endscreen/assets/landscape_0005_6_background.png");
+        this.load.image("bg1", "endscreen/assets/landscape_0000_1_trees.png");
+        this.load.image("bg2", "endscreen/assets/landscape_0001_2_trees.png");
+        this.load.image("bg3", "endscreen/assets/landscape_0002_3_trees.png");
+        this.load.image("bg4", "endscreen/assets/landscape_0003_4_mountain.png");
+        this.load.image("bg5", "endscreen/assets/landscape_0004_5_clouds.png");
+        this.load.image("bg6", "endscreen/assets/landscape_0005_6_background.png");
     }
 
     create() {
